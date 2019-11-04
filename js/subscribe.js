4938f47bb440ff455ebf8ff991759b0426aec181
Adam <adam.konyari@gmail.com>
2019-11-04 16:26:24 +0100

    validation updated

--- js/subscribe.js
+++ js/subscribe.js
@@ -8,6 +8,7 @@ class Subscribe {
 
     initSubscribe() {
         this.initCustomModal();
+        this.initSubscribeForm();
     }
 
     /**
@@ -18,25 +19,38 @@ class Subscribe {
     initCustomModal() {
         const modal = $.fn.modal,
             defaults = $.extend({}, $.fn.modal.defaults);
-        $.fn.customModal = function(options) {
-            options = $.extend( defaults, options );
+        $.fn.customModal = function (options) {
+            options = $.extend(defaults, options);
             console.log('custom modal called');
-            return modal.call( this, options );
+            return modal.call(this, options);
         };
         $('#subscribe-modal-link').on('click', () => {
             $('#modal-subscribe-div').customModal();
         });
-        $("#subscribe-form").validate({
-            rules: {
-                "first-name": {
-                    required: true,
-                    minlength: 3
-                },
-                "last-name": {
-                    required: true,
-                    minlength: 3
+    }
+
+    initSubscribeForm() {
+        //add validation to the subscribe form
+        $("#subscribe-form")
+            .validate({
+                rules: {
+                    "first-name": {
+                        required: true,
+                        minlength: 3
+                    },
+                    "last-name": {
+                        required: true,
+                        minlength: 3
+                    },
+                    email: {
+                        required: true
+                    }
                 }
-            }
+            });
+
+        $("#subscribe-form").on('submit', (e) => {
+            e.preventDefault();
+            console.log('form ok');
         });
     }
 }
