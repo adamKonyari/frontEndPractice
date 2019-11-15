b13f63417d1168d9dd4dd9e906a040c628a89bc5
adamKonyari <adam.konyari@gmail.com>
2019-11-15 15:29:12 +0100

    final commit 20191115

--- js/photos.js
+++ js/photos.js
@@ -25,20 +25,29 @@ class Photos {
      */
     openPhotoSwipe() {
         const pswpElement = document.querySelectorAll('.pswp')[0];
-        $('.gallery-container').append('<div class="photoSwipe_innerthumbs"></div>'); // div for the inner thumbnails
+        $('.gallery-container').append('<div class="photoSwipe_innerthumbs"></div>', ); // div for the inner thumbnails
 
         const options = {
             index: 0, // start at first slide
             preload: [1, 1], // only 1 image is preloaded in both direction
-            bgOpacity: 0.5
+            bgOpacity: 0.5,
+            modal: false
         };
 
         const gallery = new PhotoSwipe(pswpElement, PhotoSwipeUI_Default, this.items, options);
         gallery.init();
+        $('.gallery-container').append(`<div class="photoSwipe_imgTitle"><h1>${gallery.currItem.title}</h1></div>`)
 
         // Gallery starts closing
         gallery.listen('close', function () {
             $('.photoSwipe_innerthumbs').remove();
+            $('.photoSwipe_imgTitle').remove();
+        });
+        gallery.listen('beforeChange', function () {
+            if ($('.photoSwipe_imgTitle')) {
+                $('.photoSwipe_imgTitle').remove()
+            }
+            $('.pswp__img').append(`<div class="photoSwipe_imgTitle"><h1>${gallery.currItem.title}</h1></div>`)
         });
 
         //Create thumbnail images
@@ -63,14 +72,15 @@ class Photos {
      */
     buildItemsArray() {
         const items = [];
-        for (let i = 0; i < 15; i++) {
+        for (let i = 0; i < 50; i++) {
             const width = Math.floor((Math.random() * 1080) + 840),
                 height = Math.floor((Math.random() * 1080) + 840),
-                imgSrc = 'http://placekitten.com/' + width + '/' + height
+                imgSrc = 'http://placekitten.com/' + width + '/' + height;
             items.push({
                 src: imgSrc,
                 w: width,
-                h: height
+                h: height,
+                title: 'Cat #' + (i+1)
             });
         }
         return items;
